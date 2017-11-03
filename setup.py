a339843dd1718059e9a286b1e6f147bae5656a31
Hervé Beraud <herveberaud.pro@gmail.com>
2017-11-03 13:36:44 +0100

    update project with latest setuptools best practices
    remove setuptools from readme

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='curequests',
-    version='0.0.1',
-    description='Curio + Requests: Async HTTP for Humans',
-    long_description=__doc__,
-    author='guyskk',
-    author_email='guyskk@qq.com',
-    url='https://github.com/guyskk/curequests',
-    license='MIT',
-    packages=['curequests'],
-    install_requires=[
-        'httptools',
-        'yarl',
-        'curio',
-        'requests',
-    ],
-    zip_safe=False,
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
-)
+setup()
