57642b3b7b95fa2277356c2fdb4002bc81390443
guyskk <guyskk@qq.com>
2017-11-22 22:17:51 +0800

    release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='curequests',
-    version='0.1.0',
+    version='0.2.0',
     description='Curio + Requests: Async HTTP for Humans',
     long_description=__doc__,
     author='guyskk',
